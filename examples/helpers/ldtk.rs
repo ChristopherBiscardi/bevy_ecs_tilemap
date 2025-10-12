5dc1dc28d8d3c8b32bcaa8a49cddaa8f9b01f06b
Brandon Elam Barker <brandon.barker@gmail.com>
2025-10-12 07:24:18 -0400

    revert some change of entity construction

--- examples/helpers/ldtk.rs
+++ examples/helpers/ldtk.rs
@@ -7,7 +7,6 @@ use bevy_ecs_tilemap::{
 use std::{collections::HashMap, io::ErrorKind};
 use thiserror::Error;
 
-use bevy::prelude::MessageReader;
 use bevy::{asset::io::Reader, reflect::TypePath};
 use bevy::{
     asset::{AssetLoader, AssetPath, LoadContext},
